84a9bf776cda6ccad4496a746ccf99370112edd2
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2022-09-17 04:32:36 +0800

    first change

--- entry.js
+++ entry.js
@@ -6,12 +6,13 @@ var canvas = document.getElementById("canvas"),
   cw = window.innerWidth,
   ch = window.innerHeight,
   charArr = [
-    "   Seduction of the necrotic",
-    "a malicious glimpse   ",
+    "做愛的兄弟",
+    "         Seduction of the necrotic",
+    "a malicious glimpse          ",
     "carnality  we ignite",
-    "   constellation inversed",
-    "fetid laceration   ",
-    "f",
+    "       constellation inversed",
+    "fetid laceration       ",
+    "做愛的兄弟",
     "g",
     "h",
     "i",
@@ -35,7 +36,7 @@ var canvas = document.getElementById("canvas"),
   ],
   maxCharCount = 100,
   fallingCharArr = [],
-  fontSize = 10,
+  fontSize = 20,
   maxColums = cw / fontSize;
 canvas.width = canvas2.width = cw;
 canvas.height = canvas2.height = ch;
@@ -55,13 +56,13 @@ function Point(x, y) {
 
 Point.prototype.draw = function (ctx) {
   this.value = charArr[randomInt(0, charArr.length - 1)].toUpperCase();
-  this.speed = randomFloat(1, 5);
+  this.speed = randomFloat(1, 10);
 
   ctx2.fillStyle = "rgba(255,255,255,0.8)";
   ctx2.font = fontSize + "px san-serif";
   ctx2.fillText(this.value, this.x, this.y);
 
-  ctx.fillStyle = "#0F0";
+  ctx.fillStyle = "white";
   ctx.font = fontSize + "px san-serif";
   ctx.fillText(this.value, this.x, this.y);
 
@@ -72,7 +73,7 @@ Point.prototype.draw = function (ctx) {
   }
 };
 
-for (var i = 0; i < maxColums; i++) {
+for (var i = 0; i < maxColums; i = i + 10) {
   fallingCharArr.push(new Point(i * fontSize, randomFloat(-500, 0)));
 }
 
@@ -93,3 +94,7 @@ var update = function () {
 };
 
 update();
+
+var nickname = prompt("請輸入你的暱稱");
+
+alert("Hello " + nickname);
